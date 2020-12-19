d68881544e8073821806707c0899802797b43446
Liyeira Anaya <75145982+lmanaya@users.noreply.github.com>
2020-12-19 13:25:09 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Estructura Evaluacion semana 4
+
+#  Comando
+```
+npm install
+```
+funcionan las pruebas
+```
+Aún no está conectado a heroku
---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+const tokenServices = require('../services/token.js');
+module.exports = {
+    verificarVendedor: async (req, res, next) => {
+        if (!req.headers.token) {
+            return res.status(404).send({
+                message: 'There is not token'
+            })
+        }
+        else {
+            const response = await tokenServices.decode(req.headers.token);
+            if (response.rol === 'Administrador' || response.rol === 'Vendedor') {
+                next();
+            }
+            else {
+                return res.status(403).send({
+                    message: 'Unauthorized user'
+                })
+            }
+        }
+    }
+}
